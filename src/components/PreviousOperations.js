76874e1c01cf0892c221d9cd574c0ec28a7183ac
Erez Nagar <erez.nagar@gmail.com>
2022-02-02 23:58:16 -0500

    changing order of previous operation tiles

--- src/components/PreviousOperations.js
+++ src/components/PreviousOperations.js
@@ -8,20 +8,23 @@ const PreviousOperations = (props) => (
     <Row gutter={[24, 24]}>
       <Col xs={24} sm={12} md={8} lg={6}>
         <Tile
-          title={"Operation Cast Lead"}
-          subtitle={"27 Dec 2008 - 18 Jan 2009"}
-          fromDate={"2008-12-27"}
-          toDate={"2009-01-18"}
+          title={"Operation Guardian of the Walls"}
+          subtitle={"10 May 2021 - 21 May 2021"}
+          fromDate={"2021-05-10"}
+          toDate={"2021-05-21"}
           alertsClient={props.alertsClient}
+          showAverage
         />
       </Col>
+
       <Col xs={24} sm={12} md={8} lg={6}>
         <Tile
-          title={"Operation Pillar of Defense"}
-          subtitle={"14 Nov 2012 - 21 Nov 2012"}
-          fromDate={"2012-11-14"}
-          toDate={"2012-11-21"}
+          title={"Operation Black Belt"}
+          subtitle={"12 Nov 2019 - 16 Nov 2019"}
+          fromDate={"2019-11-12"}
+          toDate={"2019-11-16"}
           alertsClient={props.alertsClient}
+          showAverage
         />
       </Col>
       <Col xs={24} sm={12} md={8} lg={6}>
@@ -36,22 +39,20 @@ const PreviousOperations = (props) => (
       </Col>
       <Col xs={24} sm={12} md={8} lg={6}>
         <Tile
-          title={"Operation Black Belt"}
-          subtitle={"12 Nov 2019 - 16 Nov 2019"}
-          fromDate={"2019-11-12"}
-          toDate={"2019-11-16"}
+          title={"Operation Pillar of Defense"}
+          subtitle={"14 Nov 2012 - 21 Nov 2012"}
+          fromDate={"2012-11-14"}
+          toDate={"2012-11-21"}
           alertsClient={props.alertsClient}
-          showAverage
         />
       </Col>
       <Col xs={24} sm={12} md={8} lg={6}>
         <Tile
-          title={"Operation Guardian of the Walls"}
-          subtitle={"10 May 2021 - 21 May 2021"}
-          fromDate={"2021-05-10"}
-          toDate={"2021-05-21"}
+          title={"Operation Cast Lead"}
+          subtitle={"27 Dec 2008 - 18 Jan 2009"}
+          fromDate={"2008-12-27"}
+          toDate={"2009-01-18"}
           alertsClient={props.alertsClient}
-          showAverage
         />
       </Col>
     </Row>
