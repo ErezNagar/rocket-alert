05afc09120260adb17d46497922edde630b06716
Erez Nagar <erez.nagar@gmail.com>
2022-02-08 23:09:41 -0500

    adding faq content

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -3,6 +3,59 @@
 const FAQ = () => (
   <section className="section faq">
     <h2>Frequently Asked Questions</h2>
+    <div>
+      <p className="question-title">Where are these rockets launched from?</p>
+      <p className="question-answer">
+        Mostly from the Gaza Strip, although sometimes there are rockets
+        launched from South Lebanon or Syria.
+      </p>
+    </div>
+    <div>
+      <p className="question-title">Who is shooting these rockets?</p>
+      <p className="question-answer">
+        Palestinian factions, namely{" "}
+        <a hreg="https://www.dni.gov/nctc/groups/hamas.html">Hamas</a> and
+        Islamic Jihad terror organizations from Gaza, and{" "}
+        <a href="https://www.dni.gov/nctc/groups/hizballah.html">Hezbollah</a>{" "}
+        terror organization based in South Lebanon.
+      </p>
+    </div>
+    <div>
+      <p className="question-title">Where are these rockets targeted?</p>
+      <p className="question-answer">
+        Whether launched from Gaza or South Lebanon, these rockets are launched
+        indiscriminately at Israeli towns and cities, deliberatly targeting
+        innocent civilians.
+      </p>
+    </div>
+    <div>
+      <p className="question-title">Does Israel retaliate/respond?</p>
+      <p className="question-answer">
+        In addition to protecting its civilians via a combination of bomb
+        shelters and the deployment of the Iron Dome system, Israel typically
+        responds by air striking terror targets, including launch sites, command
+        and control centers, and weapons storage sites.
+      </p>
+    </div>
+    <div>
+      <p className="question-title">What is Iron Dome</p>
+      <p className="question-answer">
+        The{" "}
+        <a href="https://en.wikipedia.org/wiki/Iron_Dome">
+          Iron Dome Aerial Defense System
+        </a>{" "}
+        is an Israeli air defense system desgined to intercept and destroy
+        rockets whos trjectory would take them to an Israeli populated area.
+      </p>
+    </div>
+    <div>
+      <p className="question-title">Do rockets ever hit Israel?</p>
+      <p className="question-answer">
+        Yes. While Iron Dome intercepts about 90% of rocekts aim at Israeli
+        population, rockets still penetrate the defense system and hit
+        residential areas{" "}
+      </p>
+    </div>
   </section>
 );
 
