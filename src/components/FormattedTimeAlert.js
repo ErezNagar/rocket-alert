a1cf3848cf7c4298851fdf20266af922505abfc0
Erez Nagar <erez.nagar@gmail.com>
2022-08-06 19:43:59 -0400

    using local time

--- src/components/FormattedTimeAlert.js
+++ src/components/FormattedTimeAlert.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { parseISO, format } from "date-fns";
+import { zonedTimeToUtc } from "date-fns-tz";
 
-const FormattedTimeAlert = ({ alert }) => (
-  <>{format(parseISO(alert.timeStamp), "HH:mm")} </>
+const FormattedTimeAlert = ({ alert, toLocalTime = true }) => (
+  <>
+    {toLocalTime
+      ? format(zonedTimeToUtc(alert.timeStamp, "Asia/Jerusalem"), "HH:mm")
+      : format(parseISO(alert.timeStamp), "HH:mm")}
+  </>
 );
 
 FormattedTimeAlert.propTypes = {
