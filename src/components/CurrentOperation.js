bba7f7c5864e43554acc3dae1ef0de60cace982c
Erez Nagar <erez.nagar@gmail.com>
2022-08-09 00:07:42 -0400

    update breaking Dawn tile duration

--- src/components/CurrentOperation.js
+++ src/components/CurrentOperation.js
@@ -8,8 +8,9 @@ const CurrentOperation = (props) => (
       <Col xs={24} sm={12} md={8} lg={6}>
         <Tile
           title={"Operation Breaking Dawn"}
-          subtitle={"Started Aug 5"}
+          subtitle={"Aug 5 2022 - Aug 7 2022"}
           fromDate={new Date("2022-08-05T00:00")}
+          toDate={new Date("2022-08-08T00:00")}
           alertsClient={props.alertsClient}
           showAverage
         />
