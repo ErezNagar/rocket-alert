e27b0753fe21123149785f6e8f87d661f4f38bc2
Erez Nagar <erez.nagar@gmail.com>
2023-10-30 10:45:13 -0400

    showing alert location geographic area on map

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/).
 
+## [1.3.0](https://github.com/ErezNagar/code-red/releases/tag/v1.3.0) - 10/30/2023
+
+Added:
+
+- Map is now showing location area of most recent alert
+
 ## [1.2.0](https://github.com/ErezNagar/code-red/releases/tag/v1.2.0) - 10/29/2023
 
 Added:
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import React from "react";
+import polygons from "../polygons.json";
 
 class Map extends React.Component {
   componentDidMount() {
@@ -31,7 +32,25 @@ class Map extends React.Component {
 
       bounds.extend(new window.google.maps.LatLng(alert.lat, alert.lon));
 
-      // Set marker
+      // Collect alert coordinates
+      const coords = polygons[alert.taCityId]?.map(([lat, lng]) => {
+        const point = new window.google.maps.LatLng(lat, lng);
+        bounds.extend(point);
+        return point;
+      });
+
+      // Set alert polygon
+      const polygon = new window.google.maps.Polygon({
+        paths: coords,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.7,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.3,
+      });
+      polygon.setMap(map);
+
+      // Set alert marker
       new window.google.maps.Marker({
         position: new window.google.maps.LatLng(alert.lat, alert.lon),
         map: map,
