04c38f5749103cfa03cbad3e8a06e430d8110193
Erez Nagar <erez.nagar@gmail.com>
2025-02-16 18:54:24 -0500

    initial commit

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -6,6 +6,7 @@ import { LoadingOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
 import FadeIn from "./FadeIn";
 import Tracking from "../tracking";
 import Util from "../util";
+import Sparkline from "./Sparkline";
 
 const LoadingTile = ({ showAverage }) => (
   <Row
@@ -37,6 +38,7 @@ export default class Tile extends React.Component {
     fromDate: PropTypes.instanceOf(Date).isRequired,
     toDate: PropTypes.instanceOf(Date),
     alertsClient: PropTypes.object,
+    sparklineData: PropTypes.array,
     showAverage: PropTypes.bool,
     // "Static" tile uses alertCount instead of making a request to the server
     isStatic: PropTypes.bool,
@@ -55,7 +57,15 @@ export default class Tile extends React.Component {
     alertTypeId: Util.ALERT_TYPE_ROCKETS,
   };
 
-  state = { isLoading: true, isError: false, alerts: null, average: 0 };
+  state = {
+    isLoading: true,
+    isSparklineLoading: false,
+    sparklineRocketData: [],
+    sparklineUAVData: [],
+    isError: false,
+    alerts: null,
+    average: 0,
+  };
 
   componentDidMount() {
     if (this.props.isStatic) {
@@ -68,6 +78,7 @@ export default class Tile extends React.Component {
       });
     } else {
       this.getAlerts();
+      this.getDailyAlerts();
     }
   }
 
@@ -93,6 +104,50 @@ export default class Tile extends React.Component {
       });
   };
 
+  getDailyAlerts = () => {
+    this.setState({ isSparklineLoading: true });
+    this.props.alertsClient
+      .getTotalAlertsByDay(
+        this.props.fromDate,
+        this.props.toDate,
+        this.props.alertTypeId
+      )
+      .then((res) => {
+        const data = res.payload.map((date) => date.alerts);
+        if (this.props.alertTypeId === Util.ALERT_TYPE_ROCKETS) {
+          const sparklineData = [];
+          let count = 0;
+          for (let i = 0; i < data.length; i++) {
+            if ((i + 1) % 9 === 0) {
+              sparklineData.push(count);
+              count = 0;
+            }
+            count += data[i];
+          }
+          sparklineData.push(count);
+          this.setState({
+            sparklineData,
+            isSparklineLoading: false,
+          });
+        }
+        if (this.props.alertTypeId === Util.ALERT_TYPE_UAV) {
+          const sparklineData = data
+            .filter((_, idx) => (idx + 1) % 2 !== 0)
+            .filter((_, idx) => (idx + 1) % 2 !== 0)
+            .filter((_, idx) => (idx + 1) % 2 !== 0);
+          this.setState({
+            sparklineData,
+            isSparklineLoading: false,
+          });
+        }
+      })
+      .catch((error) => {
+        Tracking.tileError(error);
+        console.error(error);
+        this.setState({ isError: true, isSparklineLoading: false });
+      });
+  };
+
   getAverage = (total) => {
     const dayCount = differenceInDays(
       new Date(this.props.toDate),
@@ -100,6 +155,8 @@ export default class Tile extends React.Component {
     );
     return Math.round(total / dayCount);
   };
+
+  getNameId = () => this.props.title.replaceAll(" ", "_").toLowerCase();
 
   render() {
     const hasData = !this.state.isLoading && !this.state.isError;
@@ -147,6 +204,14 @@ export default class Tile extends React.Component {
               />
               {" Data unavailable"}
             </FadeIn>
+          )}
+          {(this.props.sparklineData || this.state.sparklineData) && (
+            <Sparkline
+              id={this.props.title}
+              width={250}
+              height={50}
+              data={this.props.sparklineData || this.state.sparklineData}
+            />
           )}
         </div>
       </div>
