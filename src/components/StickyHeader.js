9499076299ed54c38aeb2662634983929230cd5e
Erez Nagar <erez.nagar@gmail.com>
2025-03-31 00:01:57 -0400

    convert StickyHeader to functional component

--- src/components/StickyHeader.js
+++ src/components/StickyHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import FadeInOut from "./FadeInOut";
 import FormattedAlertTime from "./FormattedAlertTime";
@@ -7,73 +7,69 @@ import logo from "../assets/logo.svg";
 import Tracking from "../tracking";
 import Util from "../util";
 
-class StickyHeader extends React.Component {
-  state = {
-    shouldRefresh: false,
-  };
+const StickyHeader = ({
+  showStickyHeader,
+  isAlertMode,
+  realTimeAlert,
+  twitterShareText,
+}) => {
+  const [shouldRefresh, setShouldRefresh] = useState(false);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.realTimeAlert !== prevProps.realTimeAlert) {
-      this.refreshAlert(this.props.realTimeAlert);
-    }
-  }
+  useEffect(() => {
+    refreshAlert(realTimeAlert);
+  }, [realTimeAlert]);
 
-  refreshAlert = () => {
-    this.setState({
-      shouldRefresh: true,
-    });
+  const refreshAlert = () => {
+    setShouldRefresh(true);
     setTimeout(() => {
-      this.setState({ shouldRefresh: false });
+      setShouldRefresh(false);
     }, Util.REAL_TIME_ALERT_DISPLAY_DURATION);
   };
 
-  setStickyHeaderStyle = () => {
+  const setStickyHeaderStyle = () => {
     let cssClass = "sticky-header";
-    if (this.props.showStickyHeader) {
+    if (showStickyHeader) {
       cssClass += " active";
     }
-    if (this.props.isAlertMode) {
+    if (isAlertMode) {
       cssClass += " alert-mode";
     }
     return cssClass;
   };
 
-  render() {
-    const { isAlertMode, realTimeAlert } = this.props;
-    return (
-      <header className={this.setStickyHeaderStyle()}>
-        <div className="left-container">
-          <img className="logo" src={logo} alt="" />
-        </div>
-        <div className="alerts">
-          {isAlertMode && realTimeAlert && (
-            <FadeInOut show={this.state.shouldRefresh}>
-              {!Util.isSmallViewport() && (
-                <>
-                  <FormattedAlertTime timeStamp={realTimeAlert.timeStamp} />
-                  {` ${Util.getAlertTypeText(realTimeAlert)}: `}
-                </>
-              )}
-              {realTimeAlert.englishName || realTimeAlert.name}
-            </FadeInOut>
-          )}
-        </div>
-        {this.props.twitterShareText && (
-          <div className="right-container">
-            <a
-              href={`https://twitter.com/share?text=${this.props.twitterShareText}&url=RocketAlert.live&hashtags=RocketAlert,IsraelUnderAttack`}
-              target="_blank"
-              rel="noreferrer"
-              onClick={Tracking.shareStickyHeaderClick}
-            >
-              <TwitterLogo style={{ height: "36px" }} />
-            </a>
-          </div>
+  return (
+    <header className={setStickyHeaderStyle()}>
+      <div className="left-container">
+        <img className="logo" src={logo} alt="" />
+      </div>
+      <div className="alerts">
+        {isAlertMode && realTimeAlert && (
+          <FadeInOut show={shouldRefresh}>
+            {!Util.isSmallViewport() && (
+              <>
+                <FormattedAlertTime timeStamp={realTimeAlert.timeStamp} />
+                {` ${Util.getAlertTypeText(realTimeAlert)}: `}
+              </>
+            )}
+            {realTimeAlert.englishName || realTimeAlert.name}
+          </FadeInOut>
         )}
-      </header>
-    );
-  }
-}
+      </div>
+      {twitterShareText && (
+        <div className="right-container">
+          <a
+            href={`https://twitter.com/share?text=${twitterShareText}&url=RocketAlert.live&hashtags=RocketAlert,IsraelUnderAttack`}
+            target="_blank"
+            rel="noreferrer"
+            onClick={Tracking.shareStickyHeaderClick}
+          >
+            <TwitterLogo style={{ height: "36px" }} />
+          </a>
+        </div>
+      )}
+    </header>
+  );
+};
 
 StickyHeader.propTypes = {
   showStickyHeader: PropTypes.bool,
