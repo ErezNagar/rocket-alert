0ee57bf47680d39a524466690782e0a80525648d
Erez Nagar <erez.nagar@gmail.com>
2025-04-02 23:16:29 -0400

    convert RecentAlertMap to functional component

--- src/components/RecentAlertsMap.js
+++ src/components/RecentAlertsMap.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Cache from "../cache";
 import Tracking from "../tracking";
@@ -7,86 +7,74 @@ import RecentAlertsStaticMap from "./RecentAlertsStaticMap";
 import RecentAlertsInteractiveMap from "./RecentAlertsInteractiveMap";
 import withIsVisibleHook from "./withIsVisibleHook";
 
-class RecentAlertsMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.staticMapContainerRef = React.createRef();
-  }
+const RecentAlertsMap = ({ alerts, mapFocus, isIntersectingRef }) => {
+  const [polygons, setPolygons] = useState({});
+  const shouldShowInteractiveMap =
+    process.env.REACT_APP_IS_MAP_INTERACTIVE === "true";
+  const shouldShowStaticMap = process.env.REACT_APP_IS_MAP_STATIC === "true";
 
-  state = {
-    shouldShowInteractiveMap:
-      process.env.REACT_APP_IS_MAP_INTERACTIVE === "true",
-    shouldShowStaticMap: process.env.REACT_APP_IS_MAP_STATIC === "true",
-    polygons: {},
-  };
-
-  componentDidMount() {
-    this.loadPolygons();
-  }
-
-  loadPolygons = () => {
+  const loadPolygons = () => {
     if (Cache.canUseCache()) {
-      this.loadPolygonsFromCache();
+      loadPolygonsFromCache();
     } else {
       Tracking.polygonCache("cant_use_cache");
-      this.loadPolygonsFromFile().then((json) => {
-        this.setState({ polygons: json });
-      });
+      loadPolygonsFromFile();
     }
   };
 
-  loadPolygonsFromCache = () => {
-    let polygons;
-    if ((polygons = Cache.getJSON("polygons")) === null) {
-      Tracking.polygonCache("miss");
-      this.loadPolygonsFromFile().then((json) => {
-        polygons = json;
-        this.setState({ polygons });
-        this.setPolygonsToCache(polygons);
-      });
-    } else {
+  const loadPolygonsFromCache = () => {
+    let polygons = Cache.getJSON("polygons");
+    if (polygons) {
       if (Cache.get(Util.POLYGON_VERSION_KEY) === Util.POLYGON_VERSION_VALUE) {
         Tracking.polygonCache("hit");
-        this.setState({ polygons });
+        setPolygons(polygons);
       } else {
         Tracking.polygonCache("older_version");
-        this.loadPolygonsFromFile().then((json) => {
-          polygons = json;
-          this.setState({ polygons });
-          this.setPolygonsToCache(polygons);
-        });
+        loadPolygonsFromFile({ setToCache: true });
       }
+    } else {
+      Tracking.polygonCache("miss");
+      loadPolygonsFromFile({ setToCache: true });
     }
   };
 
-  setPolygonsToCache = (polygons) => {
+  const setPolygonsToCache = (polygons) => {
     Cache.setJSON("polygons", polygons);
     Cache.set(Util.POLYGON_VERSION_KEY, Util.POLYGON_VERSION_VALUE);
   };
 
-  loadPolygonsFromFile = () =>
-    import("../polygons.json").then((json) => json.default || json);
+  const loadPolygonsFromFile = ({ setToCache }) =>
+    import("../polygons.json")
+      .then((json) => json.default || json)
+      .then((polygons) => {
+        setPolygons(polygons);
+        if (setToCache) {
+          setPolygonsToCache(polygons);
+        }
+      });
 
-  render() {
-    return (
-      <section ref={this.props.isIntersectingRef} className="section map">
-        {this.state.shouldShowInteractiveMap && (
-          <RecentAlertsInteractiveMap
-            alerts={this.props.alerts}
-            polygons={this.state.polygons}
-            mapFocus={this.props.mapFocus}
-          />
-        )}
-        {this.state.shouldShowStaticMap && (
-          <RecentAlertsStaticMap
-            alerts={this.props.alerts}
-            polygons={this.state.polygons}
-          />
-        )}
-      </section>
-    );
-  }
-}
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    console.log("RecentAlertsMap mounted");
+    loadPolygons();
+  }, []);
+  /* eslint-enable react-hooks/exhaustive-deps */
+
+  return (
+    <section ref={isIntersectingRef} className="section map">
+      {shouldShowInteractiveMap && (
+        <RecentAlertsInteractiveMap
+          alerts={alerts}
+          polygons={polygons}
+          mapFocus={mapFocus}
+        />
+      )}
+      {shouldShowStaticMap && (
+        <RecentAlertsStaticMap alerts={alerts} polygons={polygons} />
+      )}
+    </section>
+  );
+};
 
 RecentAlertsMap.propTypes = {
   alerts: PropTypes.array.isRequired,
