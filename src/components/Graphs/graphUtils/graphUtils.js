20040ad9032f379cb459457e4a507590417ac5db
Erez Nagar <erez.nagar@gmail.com>
2025-05-27 18:07:34 -0400

    add Yemen missile attack time frame

--- src/components/Graphs/graphUtils/graphUtils.js
+++ src/components/Graphs/graphUtils/graphUtils.js
@@ -140,6 +140,7 @@ const isYemenMissileAttackTimeFrame = (date) => {
     [new Date("2025-05-22 11:50:00"), new Date("2025-05-22 11:53:00")],
     [new Date("2025-05-23 04:09:00"), new Date("2025-05-23 04:12:00")],
     [new Date("2025-05-25 10:49:00"), new Date("2025-05-25 10:55:00")],
+    [new Date("2025-05-27 04:59:00"), new Date("2025-05-27 05:05:00")],
   ];
 
   return (
