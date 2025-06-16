0d4f353d4781efc00c04c524a29ed6859db6e823
Erez Nagar <erez.nagar@gmail.com>
2025-06-16 00:38:53 -0400

    remove unused import

--- src/components/Header/StickyHeader.js
+++ src/components/Header/StickyHeader.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import FadeInOut from "../FadeInOut";
-import FormattedAlertTime from "../FormattedAlertTime";
 import { ReactComponent as TwitterLogo } from "../../assets/twitter.svg";
 import logo from "../../assets/logo.svg";
 import Tracking from "../../tracking";
