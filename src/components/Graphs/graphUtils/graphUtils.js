911eac01e9a910184aa69a23ad42989bae05a327
Erez Nagar <erez.nagar@gmail.com>
2025-08-04 19:11:23 -0400

    add yemen attack

--- src/components/Graphs/graphUtils/graphUtils.js
+++ src/components/Graphs/graphUtils/graphUtils.js
@@ -167,6 +167,7 @@ const isYemenMissileAttackTimeFrame = (date) => {
     [new Date("2025-07-29 20:32:00"), new Date("2025-07-29 20:34:00")],
     [new Date("2025-08-01 19:30:00"), new Date("2025-08-01 19:32:00")],
     [new Date("2025-08-03 16:33:00"), new Date("2025-08-03 16:34:00")],
+    [new Date("2025-08-05 01:00:00"), new Date("2025-08-05 01:02:00")],
   ];
 
   return (
