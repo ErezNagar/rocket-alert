958db7fa86e56e232fd52985f11c8cbe06089f66
aserper <aserper@users.noreply.github.com>
2026-03-06 23:56:02 -0500

    Switch to Stadia Alidade Smooth Dark tiles
    Better visual match to Mapbox dark-v11 with appropriate detail
    and brightness levels.

--- src/mapStyle.js
+++ src/mapStyle.js
@@ -4,13 +4,11 @@ const MAP_STYLE = {
     "carto-dark": {
       type: "raster",
       tiles: [
-        "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
-        "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
-        "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
+        "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}@2x.png",
       ],
       tileSize: 256,
       attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://stadiamaps.com/">Stadia Maps</a>',
     },
   },
   layers: [
