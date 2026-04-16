ce0035b21c8d3afe61002acb6e0e864f847f47ec
Erez Nagar <erez.nagar@gmail.com>
2026-04-15 23:45:15 -0400

    fix: remove UserLocationMap.jsx (#91)

--- src/tracking.js
+++ src/tracking.js
@@ -25,7 +25,6 @@ const Tracking = {
     }),
   timeToShelterLocationClick: () =>
     track("event", "time_to_shelter_location_click"),
-  userLocationMapLoadedEvent: () => track("event", "user_location_map_loaded"),
   graphYearClick: (year) =>
     track("event", "alert_by_day_graph_year_click", {
       year,
